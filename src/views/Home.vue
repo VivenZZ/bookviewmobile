34cbf08ceb3c7b84ce466a7fbdb7cd6fd61b1076
VivenZ <17706500512@163.com>
2019-11-17 22:29:15 +0800

    书籍列表~

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,31 +5,33 @@
         <img v-lazy="image" />
       </van-swipe-item>
     </van-swipe>
-    <div class="title">
-      <van-icon name="good-job" />
-      <h2>主编推荐</h2>
-    </div>
-    <div v-if="isLoad">
-      <van-skeleton title avatar :row="3" />
-    </div>
-    <div v-else>
-      <div v-for="item in 3" :key="item" style="margin-top:40px">
-        <p>Skeleton 骨架屏</p>
-        <p>将loading属性设置成false表示内容加载完成，此时会隐藏占位图，并显示Skeleton的子组件段落占位图宽度，
-          可传数组来设置每一行的宽度段落占位图行数头像占位图大小通过title属性显示标题占位图，通过row属性配置占位段落行数</p>
-      </div>
-    </div>
+    <book-list :data_list="recommendData" :isLoad="isLoad" :title="title"/>
+    <book-list :data_list="recommendData" :isLoad="isLoad" :title="title_01"/>
   </van-row>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
+import BookList from "../components/BookList";
 export default {
   name: 'home',
+  components: {
+    BookList
+  },
   data() {
     return {
+      title: {
+        name: '主编推荐',
+        icon: 'good-job',
+        url: '/tuj'
+      },
+      title_01: {
+        name: '畅销书本',
+        icon: 'award',
+        url: '/cxs'
+      },
+      recommendData: [],
       images: [
         'https://bossaudioandcomic-1252317822.image.myqcloud.com/activity/document/5c77b5972997fbb9ec9db6605181bc45.jpg',
         'https://bossaudioandcomic-1252317822.image.myqcloud.com/activity/document/6a87c8efce2e6525ab13c8dadccc7901.jpg',
@@ -43,14 +45,22 @@ export default {
   },
   methods: {
     async getData() {
-      let res = await this.$axios.get('http://route.showapi.com/')
-      console.log(res.data._data)
+     try {
+       let res = await this.$axios.get('http://route.showapi.com/')
+       this.recommendData = res.data.recommendData
+       console.log(res.data.recommendData)
+       this.isLoad = !this.isLoad
+     } catch (e) {
+       console.log('请求出错！')
+       console.log(e)
+     }
     }
   }
 }
 </script>
 <style lang="less" scoped>
   #home{
+    padding-bottom: 104px;
     .home_banner{
       height: 300px;
       img{
@@ -58,20 +68,7 @@ export default {
         height: 300px;
       }
     }
-    .title{
-      display: flex;
-      align-items: center;
-      color: red;
-      padding: 0 20px;
-      margin-top: 20px;
-      background-color: white;
-      i{
-        font-size: 40px;
-      }
-      h2{
-        color: #333;
-        margin-left: 20px;
-      }
-    }
+
+
   }
 </style>
